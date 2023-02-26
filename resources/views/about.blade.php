c0477e134818572df9b8acca56b3a5487ffd653c
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-26 12:58:46 +0100

    remove ;

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,11 @@
 
-@extends('layouts/main');
+@extends('layouts/main')
 @section('title','About')
 
 
 @section('content')
     
-<div class='container text-center'>
+<div class='container text-center text-white'>
     <h1>ABOUT</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ipsam distinctio? Voluptas iusto beatae saepe sapiente molestiae? Modi deleniti fugit aperiam consequuntur numquam vel amet quasi maxime, suscipit repudiandae dolorem.</p>
     </div>
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,11 +1,11 @@
 
-@extends('layouts/main');
+@extends('layouts/main')
 @section('title','Contacts')
 
 
 @section('content')
     
-<div class='container text-center'>
+<div class='container text-center text-white'>
     <h1>CONTACTS</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ipsam distinctio? Voluptas iusto beatae saepe sapiente molestiae? Modi deleniti fugit aperiam consequuntur numquam vel amet quasi maxime, suscipit repudiandae dolorem.</p>
     </div>
